7742ac99d98152edca64bb046712f203f53df2de
姚飞 <feiyao@me.com>
2020-03-19 15:37:16 +0800

    After pylint, fix some minor errors.

--- Bluetooth_Temp/read_rfcomm1.py
+++ Bluetooth_Temp/read_rfcomm1.py
@@ -5,29 +5,33 @@
 #  Send a 't' and receive 2 lines
 #
 
-import mysql.connector
 import sys
 import time
 import serial
+import mysql.connector
 
-PORT = "/dev/rfcomm1"
+if sys.argv[1]:
+    PORT = sys.argv[1]
+else:
+    PORT = "/dev/rfcomm1"
 
-user = 'www'
-pwd  = 'www'
-host = 'yfhomeserver.local'
-db   = 'yfhome'
+USER = 'www'
+PWD  = 'www'
+HOST = 'yfhomeserver.local'
+DB   = 'yfhome'
 
 # Now time
-tnow = int(time.time())   
+tnow = int(time.time())
 
 # repeat 5 times to read from rfcomm
 t = 0
-s = serial.Serial(PORT, 9600)
+SER = serial.Serial(PORT, 9600)
+
 while (t<10):
     try:
-        s.write(b't')
-        line1 = s.readline().decode('ascii').split()
-        line2 = s.readline().decode('ascii').split()
+        SER.write(b't')
+        line1 = SER.readline().decode('ascii').split()
+        line2 = SER.readline().decode('ascii').split()
 
     except serial.serialutil.SerialException:
         t += 1
@@ -35,29 +39,27 @@ while (t<10):
     else:
         break
 
-s.close
+SER.close()
 
-if (t==10) :
-    print ("RFCOMM fail")
+if t==10:
+    print("RFCOMM fail")
     sys.exit()
 
-#print(line1)
-
-cnx = mysql.connector.connect(user=user, password=pwd, host=host, database=db)
+cnx = mysql.connector.connect(USER, PWD, HOST, DB)
 cursor = cnx.cursor()
-isql = "INSERT home_temp VALUES "    
+ISQL = "INSERT home_temp VALUES "    
 
 try:
-    sql = isql + "(0, {}, 0, {}, '{}')".format(tnow, str(line1[1]), str(line1[0])) + ",(0, {}, 0, {}, '{}')".format(tnow, line2[1], line2[0])
+    sql = ISQL + "(0, {}, 0, {}, '{}')".format(tnow, str(line1[1]), str(line1[0])) \
+        + ",(0, {}, 0, {}, '{}')".format(tnow, line2[1], line2[0])
     print(sql)
         
     cursor.execute(sql)
     cnx.commit()
 
 except mysql.connector.Error as err:
-    print("insert table 'mytable' failed.")
+    print("insert table 'home_temp' failed.")
     print("Error: {}".format(err.msg))
-    sys.exit()
 
 cursor.close()
 cnx.close()
