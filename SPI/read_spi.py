337b3f3d3d977c4ca751e6bfc4d493ade7b24e4e
姚飞 <feiyao@me.com>
2020-03-31 13:09:08 +0800

    fix minor lint warning

--- SPI/read_spi.py
+++ SPI/read_spi.py
@@ -3,33 +3,32 @@
 #
 # Read MAX 31855 or MAX6675 from SPI interface and send to database
 #
-import sys
 import time
-import spidev
 import mysql.connector
+import spidev
 
 # Read from MAX6675
-def read_max6675(SPI):
+def read_max6675(bus):
 
     # Read 5 times, and use average value
     temp = 0
     for i in range(5):
-        resp = SPI.readbytes(2)
+        resp = bus.readbytes(2)
         temp += ((resp[1] + resp[0]*256) & 0x7ff8)
         time.sleep(0.2)   # min 0.18s
 
     temp /= 160 # /8/5*0.25
     return temp
 
-def read_max31855(SPI):
-    SPI.max_speed_hz = 1000000
-    resp = SPI.readbytes(4)
+def read_max31855(bus):
+    bus.max_speed_hz = 1000000
+    resp = bus.readbytes(4)
 
-    t = (resp[0] << 24) | (resp[1] << 16) | (resp[2] << 8 ) | resp[3]
+    t = (resp[0] << 24) | (resp[1] << 16) | (resp[2] << 8) | resp[3]
 
     #Internal code-juncton
-    internal = ( t >> 4) & 0x7ff
-    if (t & 0x800):
+    internal = (t >> 4) & 0x7ff
+    if t & 0x800:
         internal = internal - 4096
     internal *= 0.0625
     
