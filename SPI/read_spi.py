739ab10a1843d0eb8c2f047f42963cee7872aa64
姚 飞 <feiyao@me.com>
2021-06-03 14:03:18 +0000

    Read 7 times, and then drop a Max and a Min, then use average value

--- SPI/read_spi.py
+++ SPI/read_spi.py
@@ -9,18 +9,12 @@ import spidev
 
 # Read from MAX6675
 def read_max6675(bus):
-    # Read 5 times, and use average value
-    temp = 0
-    for i in range(5):
-        resp = bus.readbytes(2)
-        temp += (resp[1] + resp[0] << 8) & 0x7ff8
-        time.sleep(0.2)   # min 0.18s
-
-    temp /= 160 # /8/5*0.25
+    resp = bus.readbytes(2)
+    temp = (resp[1] + resp[0] << 8) & 0x7ff8
+    temp /= 32 # /8*0.25
     return temp
 
 def read_max31855(bus):
-    bus.max_speed_hz = 1000000
     resp = bus.readbytes(4)
 
     t = (resp[0] << 24) | (resp[1] << 16) | (resp[2] << 8) | resp[3]
@@ -52,13 +46,24 @@ DB   = 'yfhome'
 
 SQL  = "INSERT INTO home_temp VALUES (0, %s, 0, %s, %s )"
 
+tt = [0] * 7
+
 CNX = mysql.connector.connect(user=USER, password=PWD, host=HOST, database=DB)
 CURSOR = CNX.cursor()
 
 SPI = spidev.SpiDev()
 SPI.open(0,0)
-temp = read_max31855(SPI)
+SPI.max_speed_hz = 1000000
+
+# read 7 times
+for i in range(7):
+    tt[i] = read_max31855(SPI)
+    time.sleep(0.2)
+
 SPI.close()
+
+# drop a MAX and a MIN,  then average
+temp = (sum(tt) - max(tt) - min(tt)) / 5
 
 try:
     tnow = int(time.time())
