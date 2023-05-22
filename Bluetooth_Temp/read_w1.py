8ea315517281d0aea6f6eec1c50c27e49372d441
姚 飞 <feiyao@me.com>
2023-05-22 11:26:54 +0800

    auto detect db server

--- Bluetooth_Temp/read_w1.py
+++ Bluetooth_Temp/read_w1.py
@@ -5,29 +5,34 @@ import mysql.connector
 import sys, os
 import time
 
-user = 'www'
-pwd  = 'www'
-host = 'yfhomeserver.local'
-db   = 'yfhome'
-
-cnx = mysql.connector.connect(user=user, password=pwd, host=host, database=db)
-cursor = cnx.cursor()
+USER = 'www'
+PWD  = 'www'
+DB   = 'yfhome'
 SQL = "INSERT INTO home_temp VALUES (0, %s, 0, %s, %s) "
 
-try:
-    tnow = int(time.time())   
-    for sensor in W1ThermSensor.get_available_sensors():
-        ssum = 0.0
-        for i in range(3):
-            stemp = sensor.get_temperature()
-            ssum += stemp
-        ssum /= 3
-        val = (tnow, format(ssum, "0.1f"), "28"+sensor.id)
+HOSTLIST  = ['192.168.20.20', 'yfhomeserver.local', 'yfserver.dynv6.net']
 
-        print(val)
-        
-        cursor.execute(SQL, val)
-        cnx.commit()
+for h in HOSTLIST:
+    if os.system("ping -c 1 -w2 " + h + " > /dev/null 2>&1") == 0: 
+        HOST = h
+        break
+
+tnow = int(time.time())   
+for sensor in W1ThermSensor.get_available_sensors():
+    ssum = 0.0
+    for i in range(3):
+        stemp = sensor.get_temperature()
+        ssum += stemp
+    ssum /= 3
+    val = (tnow, format(ssum, "0.1f"), "28"+sensor.id)
+
+print(val)
+
+try:
+    cnx = mysql.connector.connect(user=USER, password=PWD, host=HOST, database=DB)
+    cursor = cnx.cursor()
+    cursor.execute(SQL, val)
+    cnx.commit()
 
 except mysql.connector.Error as err:
     print("insert table 'home_temp' failed.")
